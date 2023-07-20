1026cd7ca418fa34ebe216bca362fbcac46c3411
KhadkaRipesh <khadkanripesh@gmail.com>
2023-07-20 17:29:22 +0545

    add

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,9 +1,20 @@
-import { IsAlphanumeric, IsString } from 'class-validator';
+import {
+  IsAlphanumeric,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class SignInDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
   @IsString()
+  @MinLength(8)
+  @MaxLength(30)
   password: string;
 }
