84f70dd6ddc4eb9bbd76b92e7e7ca37a4af55b83
KhadkaRipesh <khadkanripesh@gmail.com>
2023-07-20 14:29:43 +0545

    db transaction

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { EntityManager, Repository } from 'typeorm';
+import { DataSource, EntityManager, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { BycryptService } from 'src/auth/bycrypt.service';
 export type Users = any;
@@ -10,19 +14,34 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly repoService: Repository<User>,
 
-    @InjectEntityManager() private readonly entityManager: EntityManager,
+    private datasource: DataSource,
     private bycryptService: BycryptService,
   ) {}
 
-  // async createUser(createUserDto: CreateUserDto) {
-  //   const { username, password } = createUserDto;
-  //   const hashedPassword = await this.bycryptService.hashPassword(password);
-  //   const user = this.userService.create({
-  //     username,
-  //     password: hashedPassword,
-  //   });
-  //   return await this.userService.save(user);
-  // }
+  async createUser(createUserDto: CreateUserDto) {
+    // start transaction
+    const queryRunner = this.datasource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const { username, password } = createUserDto;
+      const hashedPassword = await this.bycryptService.hashPassword(password);
+      const user = new User();
+      user.username = username;
+      user.password = hashedPassword;
+      await queryRunner.manager.save(user);
+      // commit transaction
+      await queryRunner.commitTransaction();
+      console.log('Trans committed');
+    } catch (e) {
+      // rollback transaction
+      await queryRunner.rollbackTransaction();
+      console.log('Trans rollback due to', e);
+    } finally {
+      // release transaction
+      await queryRunner.release();
+    }
+  }
 
   getUsers() {
     return this.repoService.find();
@@ -31,29 +50,50 @@ export class UsersService {
   async findUser(username: string): Promise<Users | undefined> {
     return this.repoService.findOneBy({ username: username });
   }
-
-  async createUser(createUserDto: CreateUserDto) {
-    return await this.entityManager.transaction(
-      'SERIALIZABLE',
-      async (transactionalEntityManager) => {
-        try {
-          const { username, password } = createUserDto;
-          const hashedpw = await this.bycryptService.hashPassword(password);
-          const existingUser = await transactionalEntityManager.findOne(User, {
-            where: { username },
-          });
-          if (existingUser) {
-            throw new Error('User Already exists.');
-          }
-          const user = this.repoService.create({
-            username,
-            password: hashedpw,
-          });
-          return await transactionalEntityManager.save(user);
-        } catch (e) {
-          throw e;
-        }
-      },
-    );
-  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// async createUser(createUserDto: CreateUserDto) {
+//   const { username, password } = createUserDto;
+//   const hashedPassword = await this.bycryptService.hashPassword(password);
+//   const user = this.userService.create({
+//     username,
+//     password: hashedPassword,
+//   });
+//   return await this.userService.save(user);
+// }
+
+// async createUser(createUserDto: CreateUserDto) {
+//   return await this.entityManager.transaction(
+//     'SERIALIZABLE',
+//     async (transactionalEntityManager) => {
+//       try {
+//         const { username, password } = createUserDto;
+//         const hashedpw = await this.bycryptService.hashPassword(password);
+//         const existingUser = await transactionalEntityManager.findOne(User, {
+//           where: { username },
+//         });
+//         if (existingUser) {
+//           throw new ConflictException('User exists.');
+//         }
+//         const user = this.repoService.create({
+//           username,
+//           password: hashedpw,
+//         });
+//         return await transactionalEntityManager.save(user);
+//       } catch (e) {
+//         throw e;
+//       }
+//     },
+//   );
+// }
